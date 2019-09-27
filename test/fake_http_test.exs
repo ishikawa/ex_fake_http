66ae6c415510a91772208a86ee3af15482e5e902
Takanori Ishikawa <ishikawa_takanori@mac.com>
2019-09-27 11:58:14 +0900

    raw path and query string (#9)

--- test/fake_http_test.exs
+++ test/fake_http_test.exs
@@ -44,6 +44,22 @@ defmodule FakeHTTPTest do
       assert "#{req.scheme}://#{req.host}:#{req.port}" == base_url
     end
 
+    test "trailing slash", %{server: server, base_url: base_url} do
+      FakeHTTP.Server.enqueue(server, %{message: "hello"})
+      assert {:ok, _response} = HTTPoison.get("#{base_url}/hello/")
+
+      {:ok, req} = FakeHTTP.Server.take(server)
+      assert req.request_path == "/hello/"
+    end
+
+    test "query string", %{server: server, base_url: base_url} do
+      FakeHTTP.Server.enqueue(server, %{message: "hello"})
+      assert {:ok, _response} = HTTPoison.get("#{base_url}/hello?foo=bar")
+
+      {:ok, req} = FakeHTTP.Server.take(server)
+      assert req.query_string == "foo=bar"
+    end
+
     test "given status_code integer value", %{server: server, base_url: base_url} do
       FakeHTTP.Server.enqueue(server, 200)
       assert {:ok, response} = HTTPoison.get(base_url)
